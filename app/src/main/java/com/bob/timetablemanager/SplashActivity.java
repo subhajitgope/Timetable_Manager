278212073f03829be1b8f23450fc6b0882574e0a
subhajitgope <subhajitgope5@gmail.com>
2023-12-17 23:31:57 +0530

    Initial commit

---
+++ app/src/main/java/com/bob/timetablemanager/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.bob.timetablemanager;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                }
+                finish();
+
+            }
+        },2000);
+    }
+}
